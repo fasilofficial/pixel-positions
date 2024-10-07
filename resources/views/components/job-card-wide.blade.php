17a68efe7e0a15f46b4ef841fb1c653a124802f4
fasilofficial <muhammedfasilofficial@gmail.com>
2024-10-07 18:34:33 +0530

    Welcome page UI

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-500">Laracasts</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800  transition-colors duration-300">Video Editor</h3>
+        <p class="text-sm text-gray-500 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <div>
+            <x-tag>Laravel</x-tag>
+            <x-tag>PHP</x-tag>
+            <x-tag>Blade</x-tag>
+        </div>
+    </div>
+</x-panel>
